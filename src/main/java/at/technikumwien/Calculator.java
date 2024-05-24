c69a39795c4c455e7aea5babc9e69281af5bd7c7
miicchhii <michael@langeder.org>
2024-05-24 20:37:34 +0200

    Tried adding Calculator

---
+++ src/main/java/at/technikumwien/Calculator.java
@@ -0,0 +1,11 @@
+package at.technikumwien;
+
+public class Calculator {
+    public int sum(int number1, int number2){
+        return number1+number2;
+    }
+    public int diff(int number1, int number2){
+        return number1-number2;
+    }
+
+}
