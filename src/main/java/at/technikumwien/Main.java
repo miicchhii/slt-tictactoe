5897b48f379e515776dc5e3383bc41ed4e09f60e
bernineu <wi23b089@technikum-wien.at>
2024-05-29 13:14:57 +0200

    implemented Method isFull, clear bugfix implementet UserStory feature/make-move

--- src/main/java/at/technikumwien/Main.java
+++ src/main/java/at/technikumwien/Main.java
@@ -3,8 +3,9 @@ package at.technikumwien;
 
 public class Main {
     public static void main(String[] args) {
-        Board currentboard = new Board();
-        currentboard.print();
+
+        TicTacToe tictactoe = new TicTacToe();
+        tictactoe.start();
     }
 }
 
--- src/main/java/at/technikumwien/TicTacToe.java
+++ src/main/java/at/technikumwien/TicTacToe.java
@@ -1,5 +1,8 @@
 package at.technikumwien;
 
+import javax.xml.transform.Source;
+import java.util.Scanner;
+
 public class TicTacToe {
     Player player1;
     Player player2;
@@ -8,23 +11,69 @@ public class TicTacToe {
     Board board;
 
 
-    TicTacToe(){
+    TicTacToe() {
         this.player1 = new Player('X');
         this.player2 = new Player('O');
         this.currentPlayer = player1;
         this.board = new Board();
     }
 
-    public void start(){
-        
+    public void start() {
+        //board.print();
+        Scanner scanner = new Scanner(System.in);
+
+        int inputRow, inputCol;
+
+        while (!board.isFull() && !hasWinner()) {
+
+
+            System.out.println("Current Player: " + currentPlayer.getMarker());
+            board.print();
+
+            do {
+                do {
+                    System.out.print("row (0-2):");
+                    inputRow = scanner.nextInt();
+                    System.out.print("column (0-2):");
+                    inputCol = scanner.nextInt();
+                } while ( !verifyInput(inputRow, inputCol));
+
+                if (!board.isCellEmpty(inputRow, inputCol)) {
+                    System.out.println("cell is occupied");
+                }
+
+            } while (!board.isCellEmpty(inputRow, inputCol) );
+
+
+            board.place(inputRow, inputCol, currentPlayer.getMarker());
+            switchCurrentPlayer();
+        }
+
     }
 
-    public void switchCurrentPlayer(){
+    public boolean verifyInput(int inputRow, int inputCol) {
+
+        if (inputRow < 0 || inputRow > 2) {
+            System.out.println("Invalid input");
+            return false;
+        }
+        if (inputCol < 0 || inputCol > 2) {
+            System.out.println("Invalid input");
+            return false;
+        }
+        return true;
 
     }
 
-    public void hasWinner(){
 
+    public void switchCurrentPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else currentPlayer = player1;
+    }
+
+    public boolean hasWinner() {
+        return false;
     }
 
 }
